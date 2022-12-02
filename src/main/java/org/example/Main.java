745e97f7758af4f8a416e9b1b8d1232afd40a010
Act1on04 <101579409+Act1on04@users.noreply.github.com>
2022-12-02 16:52:51 +0200

    Easy Task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,13 +3,15 @@ package org.example;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         Set<String> lines = CorpusReader.getLinesFromFile("corpus.txt");
 
-        lines.forEach(System.out::println);
+//        lines.forEach(System.out::println);
 
         // Простое задание: посчитать частоту вхождения буквы английского (26) алфавита в корпус текста.
         // Обойти каждый элемент Set
@@ -22,5 +24,14 @@ public class Main {
         // Сложное задание: символ = 1-грамма. Посчитать тоже самое для 1, 2, 3, 4, 5 -грамм
         // Частоты для n-грамм считаются по общему количеству таких же n-грамм.
         // Выводите первые 30 n-грамм по частоте встречаемости
+
+        System.out.println("\n-= Простое задание =-\n");
+        lines.stream()
+                .map(word -> word.replaceAll("[^A-Za-z]+", "").toLowerCase())
+                .flatMap(line -> Stream.of(line.split("")))
+                .collect(Collectors.toMap(key -> key, val -> 1, Integer::sum))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEach( entry -> System.out.println(entry.getKey() + " - " + entry.getValue()));
     }
 }
